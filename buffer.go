0d321b2354930a3cf7625d1895feef4147ff96f7
BLETOUS <bletous@gmail.com>
2024-02-26 17:25:57 +0100

    A

--- buffer.go
+++ buffer.go
@@ -1,24 +1,59 @@
 package event
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"sync"
+	"time"
 )
 
-type Buffer struct {
+type eventBuffer struct {
 	data    []*Event
 	maxSize int
 	mutex   sync.Mutex
 }
 
+// SaveEvent Used to Save event in DD on JSON FORMAT
+func (b *Event) saveEvent() (string, error) {
+	fileName := fmt.Sprintln(time.Now().UnixMicro())
+	filePath := BufferTmpFolder + fileName
+	f, err := os.Create(filePath)
+	defer f.Close()
+	if err != nil {
+		log.Println("CREATE EVENT DD", err)
+		return fileName, err
+	}
+	eBit, err := json.Marshal(b)
+	if err != nil {
+		log.Println("JSON EVENT DD", err)
+		return fileName, err
+	}
+	l, err := f.Write(eBit)
+	if err != nil {
+		log.Println("WRITE EVENT DD", err)
+		return fileName, err
+	}
+	fmt.Println(l, "bytes written successfully")
+	return fileName, nil
+}
+
 // Add adds an item to the buffer
-func (b *Buffer) Add(item *Event) {
+func (b *eventBuffer) Add(item *Event) {
 	b.mutex.Lock()
 	defer b.mutex.Unlock()
 
 	// Check if buffer is full
 	if len(b.data) >= b.maxSize {
-		fmt.Println("Buffer is full. Cannot add more items.")
+		// Write on disk event return on success critical on error
+		fileName, err := item.saveEvent()
+		if err != nil {
+			log.Println("BUFFER FULL AND UNABLE TO SAVE EVENT ON DISK", fileName)
+			return
+		}
 		return
 	}
 
@@ -26,15 +61,51 @@ func (b *Buffer) Add(item *Event) {
 }
 
 // Get retrieves and removes an item from the buffer
-func (b *Buffer) Get() *Event {
+func (b *eventBuffer) Get() (*Event, error) {
 	b.mutex.Lock()
 	defer b.mutex.Unlock()
 	// Check if buffer is empty
 	if len(b.data) == 0 {
-		return &Event{}
+		return &Event{}, errors.New("no more")
 	}
 	// Get and remove the first item from the buffer
 	item := b.data[0]
 	b.data = b.data[1:]
-	return item
+	return item, nil
+}
+
+// ReadEvent Used to Read event in DD
+func (b *eventBuffer) readEvent() error {
+	files, err := ioutil.ReadDir("/tmp/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		// @TODO Unmarshal file content and add it to buffer
+
+		// @TODO REMOVE
+		fmt.Println(file.Name(), file.IsDir())
+	}
+
+	return nil
+}
+
+// ShutDown is used to save on disk the all buffer memory before shutdown and loose buffer
+func (b *eventBuffer) ShutDown() {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	for {
+		item, err := b.Get()
+		// Check if buffer is empty
+		if err != nil && item.Code == 0 {
+			// No More Event
+			break
+		}
+		// Not empty write to disk and do next
+		fileName, errS := item.saveEvent()
+		if errS != nil {
+			log.Println("SHUTDOWN", "UNABLE TO SAVE EVENT ON DISK", fileName, errS)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package event
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"log"
-	"net/http"
 	"time"
 )
 
-var eventBuffer *Buffer
+var buffer *eventBuffer
 var Token string
+var BufferTmpFolder string
+var BufferErrFolder string
+var ReadFolderInterval = 1 * time.Minute
 
 type Event struct {
 	UserId     int
@@ -25,8 +23,23 @@ type Event struct {
 }
 
 func init() {
-	eventBuffer = &Buffer{maxSize: 4096}
-	eventBuffer.data = make([]*Event, 0)
+	buffer = &eventBuffer{maxSize: 4096}
+	buffer.data = make([]*Event, 0)
+	err := buffer.readEvent()
+	if err != nil {
+		// @TODO Show error on startup
+	}
+	// Init Async Reading Folder
+	go asyncRead()
+}
+func asyncRead() {
+	for {
+		err := buffer.readEvent()
+		if err != nil {
+
+		}
+		time.Sleep(ReadFolderInterval)
+	}
 }
 
 // AddEvent Used to Add event
@@ -34,46 +47,12 @@ func AddEvent(e *Event) error {
 	if Token == "" {
 		return errors.New("please set the Token var before calling AddEvent()")
 	}
-	eventBuffer.Add(e)
+	buffer.Add(e)
 	return nil
 }
 
-// SendEvent Used to Send event buffer if not empty
-func SendEvent() error {
-	if Token == "" {
-		return errors.New("please set the Token var before calling SendEvent()")
-	}
-	var events []*Event
-	for {
-		newE := eventBuffer.Get()
-		if newE.UserId == 0 {
-			break
-		} else {
-			events = append(events, newE)
-		}
-	}
-	if len(events) > 0 {
-		jsonByte, err := json.Marshal(events)
-		if err != nil {
-			fmt.Printf("Error: %s", err)
-			return err
-		}
-		postUrl := "https://in-event.critsend.io/event/received/"
-		r, err := http.NewRequest("POST", postUrl, bytes.NewBuffer(jsonByte))
-		r.Header = http.Header{"Authorization": []string{Token}}
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		client := &http.Client{}
-		res, err := client.Do(r)
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		defer res.Body.Close()
-	} else {
-		//Nothing to send
-	}
+// WriteEvent Used to Write event IN DD
+func WriteEvent(e *Event) error {
+
 	return nil
 }
